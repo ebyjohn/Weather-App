b477bd27e9a3753aeb9d1484022d3cd2181091d7
ebyjohn <ebyjohnjb@gmail.com>
2024-04-24 19:30:30 +0530

    solved some bugs regarding the index.html

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
+function getWeather() {
+    const apiKey = '28dd2641e7b88ce65f509e6f70a6cc09';
+    const city = document.getElementById('city').value;
 
-function getWeather(){
-    const apiKey='177e1b4f4ffac8cdb5a0060649736013';
-    const cirty=document.getElementById('city').ariaValueMax;
-    if(!city){
-        alert('please enter a city');
+    if (!city) {
+        alert('Please enter a city');
         return;
     }
-     const currentWeatherUrl='https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}';
-     const forecastUrl='https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}';
-    
-     fetch(currentWeatherUrl)
-           .then(response=>response.json())
-           .then(data=>{displayWeather(data);})
-           .catch(error=>{console.error('Error fetchinng current weather data:', error);
-            alert('Error fetching current weather data.please try again.');});
+
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+    fetch(currentWeatherUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            console.error('Error fetching current weather data:', error);
+            alert('Error fetching current weather data. Please try again.');
+        });
 
     fetch(forecastUrl)
-           .then(response=>response.json())
-           .then(data=>{displayHourlyForecast(data.list);})
-           .catch(error=>{console.error('Error fetchinng current weather data:', error);
-            alert('Error fetching hourly forecast data.please try again.');});
-
-
+        .then(response => response.json())
+        .then(data => {
+            displayHourlyForecast(data.list);
+        })
+        .catch(error => {
+            console.error('Error fetching hourly forecast data:', error);
+            alert('Error fetching hourly forecast data. Please try again.');
+        });
 }
+
 function displayWeather(data) {
     const tempDivInfo = document.getElementById('temp-div');
     const weatherInfoDiv = document.getElementById('weather-info');
@@ -60,6 +68,7 @@ function displayWeather(data) {
         showImage();
     }
 }
+
 function displayHourlyForecast(hourlyData) {
     const hourlyForecastDiv = document.getElementById('hourly-forecast');
 
@@ -83,6 +92,7 @@ function displayHourlyForecast(hourlyData) {
         hourlyForecastDiv.innerHTML += hourlyItemHtml;
     });
 }
+
 function showImage() {
     const weatherIcon = document.getElementById('weather-icon');
     weatherIcon.style.display = 'block'; // Make the image visible once it's loaded
